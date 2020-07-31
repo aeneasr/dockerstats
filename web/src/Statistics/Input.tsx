597ed7d3cea2aba232bdd75589bc0327b6818070
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-07-31 10:22:09 +0200

    fix: input box cleanup

--- web/src/Statistics/Input.tsx
+++ web/src/Statistics/Input.tsx
@@ -2,28 +2,24 @@ import React from 'react'
 import MuiInput from '@material-ui/core/Input'
 import IconButton from '@material-ui/core/IconButton'
 import SearchIcon from '@material-ui/icons/Search'
-import { StyledComponentProps, Theme, withStyles } from '@material-ui/core'
+import { StyledComponentProps, makeStyles } from '@material-ui/core'
 
-const styles = (theme: Theme) => ({
+const useStyles = makeStyles(theme => ({
   input: {
     color: '#015B8D',
     fontWeight: 300,
     fontSize: '43px',
     lineHeight: '56px',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: 43 / 2,
-      lineHeight: 56 / 2,
-    },
   },
   iconButton: {
     width: 56,
     height: 56,
     background: 'white',
     filter: 'drop-shadow(0px 0px 64px rgba(34, 186, 251, 0.1))',
-    [theme.breakpoints.down('sm')]: {
-      width: 56 / 2,
-      height: 56 / 2,
-    },
+    // [theme.breakpoints.down('sm')]: {
+    //   width: 27,
+    //   height: 27,
+    // },
   },
   divider: {
     width: 1,
@@ -41,11 +37,11 @@ const styles = (theme: Theme) => ({
   container: {
     flex: 1,
     marginBottom: 56,
-    [theme.breakpoints.down('sm')]: {
-      marginBottom: 56 / 2,
-    },
+    // [theme.breakpoints.down('sm')]: {
+    //   marginBottom: 27,
+    // },
   },
-})
+}))
 
 interface PropTypes extends StyledComponentProps {
   value: string
@@ -57,14 +53,8 @@ interface PropTypes extends StyledComponentProps {
   onDone(): void
 }
 
-const Input = ({
-  onChange,
-  value,
-  onKeyPress,
-  error,
-  onDone,
-  classes = {},
-}: PropTypes) => {
+const Input = ({ onChange, value, onKeyPress, error, onDone }: PropTypes) => {
+  const classes = useStyles()
   return (
     <div className={classes.container}>
       <div className={classes.box}>
@@ -92,4 +82,4 @@ const Input = ({
   )
 }
 
-export default withStyles(styles)(Input)
+export default Input
