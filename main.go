88ace69c72b21edf96ba9adadd7177ba07315936
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-12-05 16:48:25 +0100

    feat: add profiling

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/aeneasr/dockerstats/cmd"
+import (
+	"cloud.google.com/go/profiler"
+	"github.com/aeneasr/dockerstats/cmd"
+	"log"
+)
 
 func main() {
+	cfg := profiler.Config{
+		Service:        "dockerstats",
+		ServiceVersion: "0.0.0",
+		MutexProfiling: true,
+	}
+
+	if err := profiler.Start(cfg); err != nil {
+		log.Printf("Unable to start profiler: %s", err)
+	}
+
 	cmd.Execute()
 }
