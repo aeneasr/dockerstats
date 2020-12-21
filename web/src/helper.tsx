1e1ee7e95bcdb77aa373b9d73ad036d4235912a9
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-12-21 12:48:28 +0100

    fix: use corsar

--- web/src/helper.tsx
+++ web/src/helper.tsx
@@ -27,17 +27,7 @@ export const repos = ({
 }: {
   org: string
 }): Promise<{ results: [{ user: string; name: string }] }> => {
-  let url = new URL(
-    `/v2/repositories/${org}/`,
-    `${window.location.protocol}//${window.location.host}/`
-  )
-
-  if (
-    window.location.hostname === 'localhost' ||
-    window.location.hostname === '127.0.0.1'
-  ) {
-    url = new URL(`/v2/repositories/${org}/`, `https://corsar.ory.sh`)
-  }
+  const url = new URL(`/v2/repositories/${org}/`, `https://corsar.ory.sh`)
 
   url.searchParams.set('__host', 'hub.docker.com')
   url.searchParams.set('__proto', 'https')
